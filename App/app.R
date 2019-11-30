c292b1e2045e75bc9e8b85735966f641ebf00abd
oren-rimon <oren_rimonor@college.harvard.edu>
2019-11-29 20:44:22 -0500

    matching -state level

---
+++ App/app.R
@@ -0,0 +1,77 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+
+library(shiny)
+library(ggplot2)
+library(readr)
+library(readxl)
+library(Matching)
+library(janitor)
+library(tidyverse)
+
+state <- read_csv("by_state.csv")
+cz <- read_csv("by_cz.csv")
+
+quantiles <- c("1st" = state$par_q1,
+               "2nd" = state$par_q2,
+               "3rd" = state$par_q3,
+               "4th" = state$par_q4,
+               "5th" = state$par_q5)
+
+quantiles_cz <- c("1st" = cz$par_q1,
+                  "2nd" = cz$par_q2,
+                  "3rd" = cz$par_q3,
+                  "4th" = cz$par_q4,
+                  "5th" = cz$par_q5)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+    navbarPage("Inventors In The US", 
+               tabPanel("About"),
+               tabPanel("Income",
+                   navlistPanel(
+                       tabPanel("By State",
+                        fluidPage(
+                            titlePanel("By State"),
+                            p(helpText("The x-axis represents the fraction of parents in a specific 
+                                       income quintile. 1 represents the bottom quintile and 5 represents the top quintile.")),
+                            selectInput(inputId = "in_q", label = "Select an Income Quntile", 
+                                        choices = quantiles),
+                        mainPanel(
+                            plotOutput("plot1")))),
+                       tabPanel("By Commuting Zone",
+                                fluidPage(
+                                    titlePanel("By Commuting Zone"),
+                                    p(helpText("The x-axis represents the fraction of parents in a specific 
+                                       income quintile. 1 represents the bottom quintile and 5 represents the top quintile.")),
+                                    selectInput(inputId = "in_q2", label = "Select an Income Quntile", choices = quantiles_cz),
+                                    mainPanel(
+                                        plotOutput("plot2"))
+                                )),
+                       tabPanel("By College")
+                        )),
+              tabPanel("Economic Mobility")
+               )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+            
+
+    # inventors vs income quantil by state
+    output$plot1 <- renderPlot({
+        
+        ggplot(data = state, aes(x = input$in_q, y =inventor)) +
+                   geom_point() 
+    })
+        
+    # inventors vs income quantil by cz
+    output$plot2 <- renderPlot({
+            ggplot(data = cz, aes(x = input$in_q2, y =inventor)) +
+                geom_point() 
+    }) 
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
