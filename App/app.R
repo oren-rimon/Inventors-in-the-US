d370c0051dc12cd485b148180b95f822f39d7e0d
oren-rimon <oren_rimonor@college.harvard.edu>
2019-12-01 13:34:08 -0500

    adding college data

--- App/app.R
+++ App/app.R
@@ -12,6 +12,9 @@ library(tidyverse)
 
 state <- read_csv("by_state.csv")
 cz <- read_csv("by_cz.csv")
+college <- read_csv("by_college.csv")
+
+# list of choices for state income quantile
 
 quantiles <- c("1st" = state$par_q1,
                "2nd" = state$par_q2,
@@ -19,11 +22,22 @@ quantiles <- c("1st" = state$par_q1,
                "4th" = state$par_q4,
                "5th" = state$par_q5)
 
+# list of choices for commuting zone income quantile
+
 quantiles_cz <- c("1st" = cz$par_q1,
                   "2nd" = cz$par_q2,
                   "3rd" = cz$par_q3,
                   "4th" = cz$par_q4,
                   "5th" = cz$par_q5)
+
+# list of choices for college income quantile
+
+quantiles_col <- c("1st" = college$share_q1,
+                  "2nd" = college$share_q2,
+                  "3rd" = college$share_q3,
+                  "4th" = college$share_q4,
+                  "5th" = college$share_q5)
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -49,7 +63,13 @@ ui <- fluidPage(
                                     mainPanel(
                                         plotOutput("plot2"))
                                 )),
-                       tabPanel("By College")
+                       tabPanel("By College",
+                                fluidPage(
+                                    titlePanel("By College"),
+                                    p(helpText("The x-axis represents the fraction of students with parents in a specific 
+                                       income quintile. 1 represents the bottom quintile and 5 represents the top quintile.")),
+                                    selectInput(inputId = "in_q3", label = "Select an Income Quntile", choices = quantiles_col))
+                                )
                         )),
               tabPanel("Economic Mobility",
                     navlistPanel(
